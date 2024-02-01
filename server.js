535b180dba5299e55d9e9caa388f4a3afb4bdaee
baraneldemir <brneldemirrr@gmail.com>
2024-02-01 13:03:52 +0000

    baran

---
+++ server.js
@@ -0,0 +1,23 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const budgetSchema = new mongoose.Schema({
+    name: String,
+    max: Number,
+})
