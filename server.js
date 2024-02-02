f5a4ab766465cf674c98c3c0f2caedf163134d51
Alex <alexstocking@hotmail.co.uk>
2024-02-02 10:34:52 +0000

    CRUDable product database

--- server.js
+++ server.js
@@ -21,5 +21,90 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
+    admin: Boolean
+})
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    stock: Number,
+    image: String
+})
+
+const cartSchema = new mongoose.Schema({
+    products: String,
+    amount: Number,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+const Product = mongoose.model("Product", productSchema)
+const Cart = mongoose.model("Cart", cartSchema)
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Cosmic Backend Working"
+    })
+})
+
+app.get('/products', async (req, res) => {
+    try {
+        const allProducts = await Product.find({})
+        res.json(allProducts)
+    } catch(e) {
+        console.error(e)
+    }
+})
+
+app.post('/products/new', (req, res) => {
+    const product = req.body
+    const newProduct = new Product({
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        stock: product.stock,
+        image: product.image
+    })
+    newProduct.save()
+    .then(() => {
+        console.log("Product Saved")
+        res.sendStatus(200)
+    })
+    .catch(e => console.error(e))
+})
+
+app.get('/products/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    res.json(product)
+})
+
+app.delete('/products/:id', (req, res) => {
+    Product.deleteOne({"_id": req.params.id})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        res.sendStatus(500)
+    })
+})
+
+app.put('/products/:id', (req, res) => {
+    Product.updateOne({"_id": req.params.id}, {
+        name: req.body.name, 
+        description: req.body.description, 
+        price: req.body.price,
+        stock: req.body.stock,
+        image: req.body.image
+    })
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        res.sendStatus(500)
+    })
 })
 
